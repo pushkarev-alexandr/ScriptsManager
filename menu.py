790c76f393361871b6c0015c49271a21f558a27a
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2025-07-28 15:10:27 +0300

    Проверяем ScriptsManager.userFolder что существует перед добавлением в pluginPath

--- menu.py
+++ menu.py
@@ -1,7 +1,9 @@
 import nuke
+import os
 import ScriptsManager
 
 ScriptsManager.addScriptsFolderToPluginPath()  # Добавляем все папки внутри папки scripts в pluginPath для доступа к ним
 ScriptsManager.createUserDefaultSettings()  # Создает дефолтные настройки если у пользователя нет настроек
-nuke.pluginAddPath(ScriptsManager.userFolder)  # Добавим папку пользователя в plugin path чтобы оттуда загрузились менюшки(прошлый скрипт должен был создать папку и настройки если их не было)
+if os.path.isdir(ScriptsManager.userFolder):  # Добавим папку пользователя в plugin path чтобы оттуда загрузились менюшки(прошлый скрипт должен был создать папку и настройки если их не было)
+    nuke.pluginAddPath(ScriptsManager.userFolder)
 ScriptsManager.createMenu()  # Создаем менюшки для управления скриптами
