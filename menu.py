f605398c41aec2f7ed83c542f49aa0e04676a3b7
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2025-07-28 10:40:27 +0300

    В menu.py убрал версию, отформатировал коменты

--- menu.py
+++ menu.py
@@ -1,8 +1,7 @@
-#v1.5
 import nuke
 import ScriptsManager
 
-ScriptsManager.addScriptsFolderToPluginPath()#добавляем все папки внутри папки scripts в pluginPath для доступа к ним
-ScriptsManager.createUserDefaultSettings()#создает дефолтные настройки если у пользователя нет настроек
-nuke.pluginAddPath(ScriptsManager.userFolder)#добавим папку пользователя в plugin path чтобы оттуда загрузились менюшки(прошлый скрипт должен был создать папку и настройки если их не было)
-ScriptsManager.createMenu()#создаем менюшки для управления скриптами
+ScriptsManager.addScriptsFolderToPluginPath()  # Добавляем все папки внутри папки scripts в pluginPath для доступа к ним
+ScriptsManager.createUserDefaultSettings()  # Создает дефолтные настройки если у пользователя нет настроек
+nuke.pluginAddPath(ScriptsManager.userFolder)  # Добавим папку пользователя в plugin path чтобы оттуда загрузились менюшки(прошлый скрипт должен был создать папку и настройки если их не было)
+ScriptsManager.createMenu()  # Создаем менюшки для управления скриптами
